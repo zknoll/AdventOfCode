735f317cbf04d7bfef0dfd71a1eaf7052133407f
Zack Knoll <zack@mavenmachines.com>
2020-12-10 19:25:51 -0600

    2020 Day 10 part 1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,7 @@
-import adventofcode2019.day2.Day2_2019
-import adventofcode2019.day5.Day5_2019
-import adventofcode2019.day7.Day7_2019
-import days.day8.Day8
-import days.day9.Day9
+import days.day10.Day10
 
 fun main() {
     println("Hello world!")
-    Day7_2019().run()
+    Day10().run()
 }
 
--- src/main/kotlin/days/day10/Day10.kt
+++ src/main/kotlin/days/day10/Day10.kt
@@ -1,9 +1,40 @@
 package days.day10
 
 import days.Day
+import kotlin.math.abs
 
 class Day10: Day("day10_input.txt") {
-    override fun part1() = 0
+    private val conditionedInput = inputLines.map { JoltAdapters(it.toInt()) }
+
+    override fun part1() = findDifferencesOfOneOrThree(conditionedInput)
 
     override fun part2() = 0
+
+    private fun findDifferencesOfOneOrThree(adapters: List<JoltAdapters>) {
+        val sortedAdapters = adapters.sortedBy { it.outputRating }.also { }
+        val diffOne = sortedAdapters.foldIndexed(0) { index, acc, next ->
+            println("$index, $acc, ${next.outputRating}")
+            acc + if (index == 0) {
+                if (sortedAdapters[index].outputRating == 1) 1 else 0
+            } else {
+                if (abs(sortedAdapters[index].outputRating - sortedAdapters[index - 1].outputRating) == 1) 1 else 0
+            }
+        }
+        val diffThree = sortedAdapters.foldIndexed(0) { index, acc, next ->
+            println("$index, $acc, ${next.outputRating}")
+            acc + if (index == 0) {
+                if (sortedAdapters[index].outputRating == 3) 1 else 0
+            } else {
+                if (abs(sortedAdapters[index].outputRating - sortedAdapters[index-1].outputRating) == 3) 1 else 0
+            }
+        } + 1
+        println("DiffOne = $diffOne, diffThree = $diffThree, product = ${diffOne * diffThree}")
+    }
+
+    private class JoltAdapters(val outputRating: Int) {
+        val inputRatingMax = outputRating - 1
+        val inputRatingMin = outputRating - 3
+
+        fun supportsInput(input: Int) = input in inputRatingMin..inputRatingMax
+    }
 }
